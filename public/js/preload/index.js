1b6738c1f17e489432f18dd17add44bfe91cdf97
marco121942 <marco.sotomayor@tecsup.edu.pe>
2023-02-13 00:08:59 -0500

    test

--- public/js/preload/index.js
+++ public/js/preload/index.js
@@ -27,7 +27,7 @@ if (btnPreloader.length > 0) {
 
 setTimeout(() => {
     preloader.classList.add("opacity-0", "pointer-events-none");
-}, 10000);
+}, 7000);
 
 function preloadActive($type, $ruta, $periodo) {
     if ($type === "entidad") {
