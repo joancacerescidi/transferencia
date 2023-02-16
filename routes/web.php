c53770981a52237fb5671eba00dddad0eead795c
marco121942 <marco.sotomayor@tecsup.edu.pe>
2023-02-16 01:28:10 -0500

    2.- implement verify email

--- routes/web.php
+++ routes/web.php
@@ -34,13 +34,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth','verified'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
 
