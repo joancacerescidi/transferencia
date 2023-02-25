e725b7be4ccfcb0f5abcb761a60b39fa25053e31
marco121942 <marco.sotomayor@tecsup.edu.pe>
2023-02-24 21:49:40 -0500

    SSL 1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
+            $table->string('facebook_id')->unique()->nullable();
+            $table->string('gmail_id')->unique()->nullable();
             $table->enum('type', ['free', 'payment']);
             $table->timestamps();
         });
