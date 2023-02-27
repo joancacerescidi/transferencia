23588f53443ecebcc35b3a517b3794df27580e8d
marco121942 <marco.sotomayor@tecsup.edu.pe>
2023-02-27 02:12:54 -0500

    Controllers facebook and google and add inputs in table user --  v2

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -50,5 +52,49 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/');
+    }
+    public function facebook()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+    public function authFacebook()
+    {
+        $user = Socialite::driver('facebook')->user();
+
+        $userExists = User::where('facebook_id', $user->id)->where('type_auth', 'facebook')->first();
+        if ($userExists) {
+            Auth::login($userExists);
+        } else {
+            $userNew = User::created([
+                'name' => $user->name,
+                'email' => $user->email,
+                'type_auth' => 'facebook',
+                'facebook_id' => $user->id,
+                'type' => 'free',
+            ]);
+            Auth::login($userNew);
+        }
+    }
+    public function google()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+    public function authGoogle()
+    {
+        $user = Socialite::driver('google')->user();
+
+        $userExists = User::where('google_id', $user->id)->where('type_auth', 'google')->first();
+        if ($userExists) {
+            Auth::login($userExists);
+        } else {
+            $userNew = User::created([
+                'name' => $user->name,
+                'email' => $user->email,
+                'type_auth' => 'google',
+                'google_id' => $user->id,
+                'type' => 'free',
+            ]);
+            Auth::login($userNew);
+        }
     }
 }
