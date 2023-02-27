319f3cc5523105c6aa7fad40abc55615111bd0c3
marco121942 <marco.sotomayor@tecsup.edu.pe>
2023-02-26 22:58:15 -0500

    route-1

--- routes/web.php
+++ routes/web.php
@@ -34,9 +34,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
