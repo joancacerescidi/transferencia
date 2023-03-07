7788ee2c75fb84c1af51364401bc4667ae104248
marco121942 <marco.sotomayor@tecsup.edu.pe>
2023-03-07 00:11:01 -0500

    header email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'Laravel')
-                <img src="https://qqperu.com/images/qqperu.png"  alt="Qullqita Qatipay">
+                <img src="https://qqperu.com/images/qqperu.png" alt="Qullqita Qatipay">
             @else
                 {{ $slot }}
             @endif
